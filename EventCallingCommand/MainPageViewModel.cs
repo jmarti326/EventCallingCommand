db138ed4acab9e0331a42a990133bc9b0bad6d9f
Juan R. Martí Mercado <juan.marti@abarcahealth.com>
2020-01-31 23:03:21 -0400

    First Initial Commit

---
+++ EventCallingCommand/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace EventCallingCommand
+{
+    public class MainPageViewModel
+    {
+        public bool ToggleState { get; set; }
+
+        public MainPageViewModel()
+        {
+        }
+
+        public Command OnSwitchStateChange => new Command((state) =>
+        {
+            this.ToggleState = (bool)state;
+        });
+    }
+}
